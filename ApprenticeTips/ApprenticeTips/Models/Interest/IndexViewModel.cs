48462f2f7bc7671dde6b02431fabbedddc2cb5cc
Matthew Stapleton <matthew.stapleton@jbaconsulting.com>
2021-11-29 11:25:10 +0000

    Add in basic data entry fields

--- ApprenticeTips/ApprenticeTips/Models/Interest/IndexViewModel.cs
+++ ApprenticeTips/ApprenticeTips/Models/Interest/IndexViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,22 @@ namespace ApprenticeTips.App.Models.Interest
 {
     public class IndexViewModel
     {
+        [Display(Name = "Firstname:")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Surname:")]
         public string Surname { get; set; }
+
+        [Display(Name = "Email:")]
         public string Email { get; set; }
+
+        [Display(Name = "Contact Number:")]
         public string PhoneNumber { get; set; }
-        public List<string> PreviousQualifications { get; set; }
+
+        [Display(Name = "Level of Previous qualification:")]
+        public List<int> PreviousQualifications { get; set; }
+
+        [Display(Name = "Please select the routes that interest you:")]
         public List<string> ApprentishipInterests { get; set; }
     }
 }
