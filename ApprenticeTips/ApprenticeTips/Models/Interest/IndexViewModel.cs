c3639177f35f47b5215fdba798645ff8053ec36b
Matthew Stapleton <matthew.stapleton@jbaconsulting.com>
2021-11-29 10:56:44 +0000

    Create View Model

--- ApprenticeTips/ApprenticeTips/Models/Interest/IndexViewModel.cs
+++ ApprenticeTips/ApprenticeTips/Models/Interest/IndexViewModel.cs
@@ -7,5 +7,11 @@ namespace ApprenticeTips.App.Models.Interest
 {
     public class IndexViewModel
     {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<string> PreviousQualifications { get; set; }
+        public List<string> ApprentishipInterests { get; set; }
     }
 }
