40018c6a5d11db6c785109c06e83e86b6e0469b9
Matthew Stapleton <matthew.stapleton@jbaconsulting.com>
2021-11-29 15:45:09 +0000

    Add in dropdowns

--- ApprenticeTips/ApprenticeTips/Models/Internal/InternalSearchViewModel.cs
+++ ApprenticeTips/ApprenticeTips/Models/Internal/InternalSearchViewModel.cs
@@ -8,15 +8,15 @@ namespace ApprenticeTips.App.Models.Internal
     public class InternalSearchViewModel
     {
         public List<RouteViewModel> Routes { get; set; }
-        public int RouteId { get; set; }
+        public int? RouteId { get; set; }
 
         public List<StatusViewModel> Status { get; set; }
-        public int StatusId { get; set; }
+        public int? StatusId { get; set; }
 
         public List<LevelViewModel> Levels { get; set; }
-        public int LevelId { get; set; }
+        public int? LevelId { get; set; }
 
         public List<DurationViewModel> Durations { get; set; }
-        public int DurationId { get; set; }
+        public int? DurationId { get; set; }
     }
 }
