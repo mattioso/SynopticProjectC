e06571dcadc09b30ab38f2f1391d63bb44c91fdf
Matthew Stapleton <matthew.stapleton@jbaconsulting.com>
2021-11-29 10:39:45 +0000

    Add in inital files

---
+++ ApprenticeTips/ApprenticeTips/Models/Interest/IndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApprenticeTips.App.Models.Interest
+{
+    public class IndexViewModel
+    {
+    }
+}
