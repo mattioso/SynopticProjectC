513aeeb28dbe01a019732f2a8ff4075d3baa2917
Matthew Stapleton <matthew.stapleton@jbaconsulting.com>
2021-11-29 15:17:33 +0000

    Add basic files for Internal Search Functionality

---
+++ ApprenticeTips/ApprenticeTips/Models/Internal/InternalSearchViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApprenticeTips.App.Models.Internal
+{
+    public class InternalSearchViewModel
+    {
+        public List<RouteViewModel> Routes { get; set; }
+        public int RouteId { get; set; }
+
+        public List<StatusViewModel> Status { get; set; }
+        public int StatusId { get; set; }
+
+        public List<LevelViewModel> Level { get; set; }
+        public int LevelId { get; set; }
+
+        public List<DurationViewModel> Duration { get; set; }
+        public int DurationId { get; set; }
+    }
+}
