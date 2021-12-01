611b78fe3f44dc00aedcff7e78797d59b6bd4e55
Matthew Stapleton <matthew.stapleton@jbaconsulting.com>
2021-12-01 16:30:42 +0000

    Add datatable to search

--- ApprenticeTips/ApprenticeTips.App/wwwroot/js/InternalSearch.js
+++ ApprenticeTips/ApprenticeTips.App/wwwroot/js/InternalSearch.js
@@ -6,10 +6,33 @@ function Search() {
         "LevelId": document.getElementById("LevelId").value,
         "DurationId": document.getElementById("DurationId").value
     }
+    $("#error-alert").hide();
+    $("#error-alert").empty()
 
-    $.post("Internal/GetTableData",
-        data,
-        function (data, status) {
-            alert("Data: " + data + "\nStatus: " + status);
-        });
+    if (data.RouteId == -1 && data.StatusId == -1 && data.LevelId == -1 && data.DurationId == -1) {
+        $("#error-alert").append("<p>Please Select an option from one of the dropdowns</p>")
+        $("#error-alert").show();
+    }
+    else {
+        $.post("Internal/GetTableData",
+            data,
+            function (data, status) {
+                console.log(data)
+                $("#table-div").empty()
+                $('#table-div').append('<table id="search-table"></table>')
+                $('#search-table').DataTable({
+                    "data": JSON.parse(data),
+                    "columns": [
+                        { "data": "Name" },
+                        {
+                            "data": "Link",
+                            "render": function (data, type) {
+                                return `<a href="${data}">${data}</a>`
+                            }
+                        }
+                    ]
+                })
+            });
+    }
+
 }
