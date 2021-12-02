439e9125a653a4880ecea18e4bcb16d77992262c
Matthew Stapleton <matthew.stapleton@jbaconsulting.com>
2021-12-02 11:29:23 +0000

    Modify visuals for header and search modified

---
+++ ApprenticeTips/ApprenticeTips.App/wwwroot/js/internal-search.js
@@ -0,0 +1,40 @@
+﻿
+function Search() {
+    data = {
+        "RouteId": document.getElementById("RouteId").value,
+        "StatusId": document.getElementById("StatusId").value,
+        "LevelId": document.getElementById("LevelId").value,
+        "DurationId": document.getElementById("DurationId").value
+    }
+    $("#error-alert").hide();
+    $("#error-alert").empty();
+
+    if (data.RouteId == -1 && data.StatusId == -1 && data.LevelId == -1 && data.DurationId == -1) {
+        $("#error-alert").append("<p>Please Select an option from one of the dropdowns</p>");
+        $("#error-alert").show();
+    }
+    else {
+        $("#results").show();
+        $.post("Internal/GetTableData",
+            data,
+            function (data, status) {
+                $("#table-div").empty()
+                $('#table-div').append('<table id="search-table"></table>')
+
+                $('#search-table').DataTable({
+                    "data": JSON.parse(data),
+                    "iDisplayLength": 50,
+                    "columns": [
+                        { "data": "Name" },
+                        {
+                            "data": "Link",
+                            "render": function (data, type) {
+                                return `<a href="${data}">${data}</a>`
+                            }
+                        }
+                    ]
+                })
+            });
+    }
+
+}
